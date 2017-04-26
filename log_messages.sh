23bece20be161f2915d2c555b57704255775790e
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-04-26 15:52:21 -0300

    :art: Put all manual logs to be on arrows.

--- log_messages.sh
+++ log_messages.sh
@@ -6,12 +6,12 @@ SECTION='\033[1;33m'
 
 function space () {
     echo ""
-    printf "${1}${NC}\n"
+    printf "-----> ${1}${NC}\n"
     echo ""
 }
 
 function section () {
-    space "${SECTION}-----> ${1}"
+    space "${SECTION}${1}"
 }
 
 function success () {
@@ -21,5 +21,7 @@ function success () {
 function check_error () {
     if [ $1 -ne 0 ]; then
         space "${ERROR}Something went wrong"
+    else
+        space "${SUCCESS}OK"
     fi
 }
