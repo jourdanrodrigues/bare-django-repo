e595d1cebbe45b8c1a3196f6669cf347cea3663a
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-04-27 16:06:50 -0300

    :sparkles: Add dependency checking and read commands of "app.json".
    Add file to check for dependencies and install them;
    Add section to read custom commands from an "app.json" file;
    General improvements in code and messages.

---
+++ utils/check_dependencies.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+command -v jq > /dev/null # Check for JQ https://stedolan.github.io/jq/
+if [[ ${?} -ne 0 ]]; then
+  ORIGINAL_PATH=`pwd` # Save current directory
+  source ./utils/log_messages.sh
+  log "Install \"jq\""
+
+  JQ=jq-1.5 # See https://github.com/stedolan/jq/releases/ for new releases
+  if [ -d /tmp/${JQ} ]; then
+    log "Using cached"
+    cd /tmp/${JQ}
+  else
+    cd /tmp/
+    curl -OL https://github.com/stedolan/jq/releases/download/${JQ}/${JQ}.tar.gz > /dev/null
+    tar xfz ${JQ}.tar.gz
+    rm -rf ${JQ}.tar.gz
+    cd ${JQ}
+    ./configure --disable-maintainer-mode > /dev/null
+    make > /dev/null
+  fi
+  make install > /dev/null
+  check_error $?
+
+  cd ${ORIGINAL_PATH}
+fi
