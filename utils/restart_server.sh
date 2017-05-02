93437aeb3a2e0da5a1d0dcd3ba2384a5bae5b2e9
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-02 11:27:59 -0300

    :snowflake: Remove sed copy since it already creates it.

--- utils/restart_server.sh
+++ utils/restart_server.sh
@@ -3,7 +3,6 @@
 source ${UTILS_PATH}/log_messages.sh
 
 APACHE_CONF=${APP_PATH}/apache2/conf/httpd.conf
-cp ${APACHE_CONF} ${APACHE_CONF}.backup
 # Change old Python path or old Python home to this env
 sed -ire 's|python-path.*|python-home='${APP_PATH}'/env|' ${APACHE_CONF}
 sed -ire 's|python-home.*|python-home='${APP_PATH}'/env|' ${APACHE_CONF}
