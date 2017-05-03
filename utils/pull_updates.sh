7002e822db8d3d4bdedec5894ccc477e80a663f7
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-03 00:17:58 -0300

    :pencl: Change git dir definition.

--- utils/pull_updates.sh
+++ utils/pull_updates.sh
@@ -3,5 +3,5 @@
 source ${UTILS_PATH}/log_messages.sh
 
 log "Pull possible updates for deployment automation"
-git --git-dir=./.git/ pull
+git --git-dir=$(pwd)/.git/ pull
 echo "" # Leave a space for the deployment log
