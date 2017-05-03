5147066732a331288948a707ba63475cd88cd0c2
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-03 03:18:27 -0300

    :ambulance: Fix if-else statement.

--- utils/install_requirements.sh
+++ utils/install_requirements.sh
@@ -5,7 +5,7 @@ source ${UTILS_PATH}/log_messages.sh
 section "Install project's requirements"
 if [ -f setup.py ]; then
   pip install -e .
-else if [ -f requirements.txt ]; then
+elif [ -f requirements.txt ]; then
   pip install -r requirements.txt
 fi
 check_error $?
