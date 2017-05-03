da271f1bcde7a8eaf53c8f39bdabf1d8583045b8
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-03 03:42:36 -0300

    :speaker: Add info about missing requirements.

--- utils/install_requirements.sh
+++ utils/install_requirements.sh
@@ -7,5 +7,7 @@ if [ -f setup.py ]; then
   pip install -e .
 elif [ -f requirements.txt ]; then
   pip install -r requirements.txt
+else
+  log "The project does not have a \"requirements.txt\" or a \"setup.py\"."
 fi
 check_error $?
