53295a96422f24d91c5f092f686e43955768bb40
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-04 09:58:02 -0300

    :sparkles: Improve dependency checking.

--- utils/check_dependencies.sh
+++ utils/check_dependencies.sh
@@ -7,16 +7,17 @@ ORIGINAL_PATH=`pwd` # Save current directory
 source ${UTILS_PATH}/log_messages.sh
 
 # Install/Upgrade pip
-if [[ $(command -v pip) > /dev/null ]] && [[ ! "$(pip --version)" =~ '^pip 9\.0\.1' ]]; then
+if [[ ! $(echo "command -v pip") > /dev/null ]] || [[ ! $(pip --version) =~ $(echo "^pip (9|1[0-9])\.[0-9]+\.[1-9]") ]]; then
+  log "Install/upgrade \"pip\""
   cd ${TMP_PATH}
   curl -OL https://bootstrap.pypa.io/get-pip.py
-  python2.7 get-pip.py > /dev/null
+  python2.7 get-pip.py
   rm -rf get-pip.py
   cd ${ORIGINAL_PATH}
 fi
 
-command -v ${BIN_PATH}/jq > /dev/null # Check for JQ https://stedolan.github.io/jq/
-if [[ ${?} -ne 0 ]]; then
+# Check for JQ https://stedolan.github.io/jq/
+if [[ $(command -v ${BIN_PATH}/jq) > /dev/null ]]; then
   log "Install \"jq\""
   JQ=jq-1.5 # See https://github.com/stedolan/jq/releases/ for new releases
   if [ -d ${TMP_PATH}/${JQ} ]; then
@@ -37,10 +38,10 @@ if [[ ${?} -ne 0 ]]; then
   cd ${ORIGINAL_PATH}
 fi
 
-command -v virtualenv > /dev/null # Check for VirtualEnv https://github.com/pypa/virtualenv
-# Note: didn't use with ${BIN_PATH} cause there is not a customization installation doesn't set it and
-# in the server, it points to the right bin path
-if [[ ${?} -ne 0 ]]; then
+# Check for VirtualEnv https://github.com/pypa/virtualenv
+# Note: didn't use with ${BIN_PATH} cause there is not a way to perform a custom installation to set it, and
+# local and server points to the right bin path by default
+if [[ $(command -v virtualenv) > /dev/null ]]; then
   log "Install \"virtualenv\""
   VENV_VERSION=15.1.0 # See https://github.com/pypa/virtualenv/releases for new releases
   VENV_PATH=virtualenv-${VENV_VERSION}
