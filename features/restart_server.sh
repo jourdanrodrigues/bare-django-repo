0ceb291267f39e51acde0b99a732c56402036b9a
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-04 18:40:08 -0300

    :snowflake: Detect "sed" location.

--- features/restart_server.sh
+++ features/restart_server.sh
@@ -3,8 +3,16 @@
 source ${UTILS_PATH}/log_messages.sh
 
 APACHE_CONF=${APP_PATH}/apache2/conf/httpd.conf
-# Change old Python path or old Python home to this env
-sed -ire 's|python-path.*|python-path='${PROJECT_PATH}' python-home='${APP_PATH}'/env|;s|WSGIScriptAlias /.*|WSGIScriptAlias / '$(find ${PROJECT_PATH} -name "wsgi.py")'|' ${APACHE_CONF}
+
+if [ -f /usr/bin/sed ]; then SED_BIN=/usr/bin/sed
+elif [ -f /bin/sed ]; then SED_BIN=/bin/sed
+fi
+# Change old Python path or old Python home to this project configuration
+if [ ${SED_BIN} ]; then
+  ${SED_BIN} -ire 's|python-path.*|python-path='${PROJECT_PATH}' python-home='${APP_PATH}'/env|;s|WSGIScriptAlias /.*|WSGIScriptAlias / '$(find ${PROJECT_PATH} -name "wsgi.py")'|' ${APACHE_CONF}
+else
+  log "\"sed\" not found! Apache configuration not changed."
+fi
 
 section "Restart Apache server"
 ${APP_PATH}/apache2/bin/restart || exit 1
