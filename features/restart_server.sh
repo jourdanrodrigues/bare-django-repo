39faf1afe2c6cb36b79e797ebc5559018bb36322
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-04 14:42:30 -0300

    :sparkles: Set python path and wsgi file on Apache.

--- features/restart_server.sh
+++ features/restart_server.sh
@@ -4,8 +4,8 @@ source ${UTILS_PATH}/log_messages.sh
 
 APACHE_CONF=${APP_PATH}/apache2/conf/httpd.conf
 # Change old Python path or old Python home to this env
-sed -ire 's|python-path.*|python-home='${APP_PATH}'/env|' ${APACHE_CONF}
-sed -ire 's|python-home.*|python-home='${APP_PATH}'/env|' ${APACHE_CONF}
+sed -ire 's|python-path.*|python-path='${PROJECT_PATH}' python-home='${APP_PATH}'/env|' ${APACHE_CONF}
+sed -ire 's|WSGIScriptAlias /.*|WSGIScriptAlias / '$(find ${PROJECT_PATH} -name "wsgi.py")'|' ${APACHE_CONF}
 
 section "Restart Apache server"
 ${APP_PATH}/apache2/bin/restart
