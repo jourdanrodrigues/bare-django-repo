fcd5f412e17c6d0f411cd8c135beacd994098c48
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-04 21:35:46 -0300

    :art: Change "sed" setting location.

--- features/restart_server.sh
+++ features/restart_server.sh
@@ -4,9 +4,6 @@ source ${UTILS_PATH}/log_messages.sh
 
 APACHE_CONF=${APP_PATH}/apache2/conf/httpd.conf
 
-if [ -f /usr/bin/sed ]; then SED_BIN=/usr/bin/sed
-elif [ -f /bin/sed ]; then SED_BIN=/bin/sed
-fi
 # Change old Python path or old Python home to this project configuration
 if [ ${SED_BIN} ]; then
   ${SED_BIN} -ire 's|python-path.*|python-path='${PROJECT_PATH}' python-home='${APP_PATH}'/env|;s|WSGIScriptAlias /.*|WSGIScriptAlias / '$(find ${PROJECT_PATH} -name "wsgi.py")'|' ${APACHE_CONF}
--- utils/set_paths.sh
+++ utils/set_paths.sh
@@ -12,3 +12,7 @@ export APP_PATH=$(dirname ${CURRENT_DIRECTORY})
 PROJECT_DIRECTORY=$(basename ${CURRENT_DIRECTORY}) # Trick to get directory name without ".git"
 export PROJECT_PATH=${APP_PATH}/${PROJECT_DIRECTORY%.git}
 mkdir -p ${PROJECT_PATH}
+
+if [ -f /usr/bin/sed ]; then export SED_BIN=/usr/bin/sed
+elif [ -f /bin/sed ]; then export SED_BIN=/bin/sed
+fi
