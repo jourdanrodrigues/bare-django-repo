2cd5da7f6f32617c2bd207e84897d3f3218439b7
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-05 02:35:56 -0300

    :sparkles: Add backup functionality.

--- utils/set_paths.sh
+++ utils/set_paths.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# "./<this_file> -b" to perform the backup
+while getopts :b opt; do [ ${opt} == b ] && BACKUP=true; done
+
 export UTILS_PATH=`pwd`/utils
 export FEATS_PATH=`pwd`/features
 
@@ -11,6 +14,10 @@ export APP_PATH=$(dirname ${CURRENT_DIRECTORY})
 
 PROJECT_DIRECTORY=$(basename ${CURRENT_DIRECTORY}) # Trick to get directory name without ".git"
 export PROJECT_PATH=${APP_PATH}/${PROJECT_DIRECTORY%.git}
+if [ ${BACKUP} ] && [ -d ${PROJECT_PATH} ]; then # Perfoms a simple backup
+  rm -rf ${PROJECT_PATH}.backup
+  mv ${PROJECT_PATH} ${PROJECT_PATH}.backup
+fi
 mkdir -p ${PROJECT_PATH}
 
 if [ -f /usr/bin/sed ]; then export SED_BIN=/usr/bin/sed
